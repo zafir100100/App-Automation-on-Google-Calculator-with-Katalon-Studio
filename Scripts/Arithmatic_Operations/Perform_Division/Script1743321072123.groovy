443f5ac0b5ff59fc26dd05b8eb87240f99d49099
zafir100100 <55599023+zafir100100@users.noreply.github.com>
2025-03-30 14:02:03 +0600

    Initial commit

---
+++ Scripts/Arithmatic_Operations/Perform_Division/Script1743321072123.groovy
@@ -0,0 +1,43 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+// Start the calculator app
+Mobile.startApplication('D:\\coding\\sqa\\espresso\\App-Automation-on-Google-Calculator-with-Selenium-Appium\\src\\test\\resources\\Calculator.apk', 
+    true)
+
+// Perform 3 + 2
+Mobile.tap(findTestObject('digit_3'), 0)
+
+Mobile.tap(findTestObject('divide_button'), 0)
+
+Mobile.tap(findTestObject('digit_2'), 0)
+
+Mobile.tap(findTestObject('equal_button'), 0)
+
+// Get result text
+String result = Mobile.getText(findTestObject('result_text_view'), 0)
+
+// Validate result is 5
+Mobile.verifyEqual(result, '1.5')
+
+// Clear and close the app
+Mobile.tap(findTestObject('all_clear_button'), 0)
+
+Mobile.closeApplication()
+
---
+++ Test Suites/Math.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Math</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>9d5bd528-899e-418e-889a-958149260917</testSuiteGuid>
+   <testCaseLink>
+      <guid>e2d627a5-ee4f-4283-ab4c-e48f7511ec50</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Arithmatic_Operations/Perform_Addition</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>94557016-37c3-4fcd-8d4e-fcd4fef8b257</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Arithmatic_Operations/Perform_Subtraction</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>158a30e2-4287-4c87-9332-9b36711924a5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Arithmatic_Operations/Perform_Multiplication</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>27a80ef9-16eb-4d04-a8a1-eb493436f00a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Arithmatic_Operations/Perform_Division</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
